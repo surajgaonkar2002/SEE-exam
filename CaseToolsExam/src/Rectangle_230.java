c1cc7b54e56ade70fc9a1d4c1041e15ddb62fb5f
admin <admin@Bcalab4-m11>
2022-06-16 09:08:00 +0530

    rbca230

---
+++ CaseToolsExam/src/Rectangle_230.java
@@ -0,0 +1,16 @@
+
+public class Rectangle_230 {
+int length;
+int width;
+
+ Rectangle_230(int lenght, int width)
+ {
+	 this.length=length;
+	 this.width=width;
+ }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
